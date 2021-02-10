38b8a67fdf30b6e8e08d77fbe4e066d87284a5fd
tianzhongsheng <tianzhongsheng@lingxi360.com>
2021-02-10 17:24:59 +0800

    修改控制器

--- src/app.php
+++ src/app.php
@@ -13,15 +13,20 @@ function is_leap_year($year = null) {
 }
 
 $routes = new Routing\RouteCollection();
+//$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
+//    'year' => null,
+//    '_controller' => function ($request) {
+//        if (is_leap_year($request->attributes->get('year'))) {
+//            return new Response('Yep, this is a leap year!');
+//        }
+//
+//        return new Response('Nope, this is not a leap year.');
+//    }
+//]));
+
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', [
     'year' => null,
-    '_controller' => function ($request) {
-        if (is_leap_year($request->attributes->get('year'))) {
-            return new Response('Yep, this is a leap year!');
-        }
-
-        return new Response('Nope, this is not a leap year.');
-    }
+    '_controller' => 'Calendar\Controller\LeapYearController::index',
 ]));
 
 return $routes;
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -10,8 +10,8 @@ class ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f
         'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-        '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        '0d59ee240a4cd96ddbb4ff164fccea4d' => __DIR__ . '/..' . '/symfony/polyfill-php73/bootstrap.php',
         '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
     );
 
@@ -97,6 +97,10 @@ class ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f
         ),
     );
 
+    public static $fallbackDirsPsr4 = array (
+        0 => __DIR__ . '/../..' . '/src',
+    );
+
     public static $classMap = array (
         'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
@@ -111,6 +115,7 @@ class ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f::$prefixDirsPsr4;
+            $loader->fallbackDirsPsr4 = ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f::$fallbackDirsPsr4;
             $loader->classMap = ComposerStaticInit08d8493c7cdb9b73b492ab624b65026f::$classMap;
 
         }, null, ClassLoader::class);

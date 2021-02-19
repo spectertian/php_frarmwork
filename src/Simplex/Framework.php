91948f03ead9f1403f935589e71622e95add92fe
tianzhongsheng <tianzhongsheng@lingxi360.com>
2021-02-19 17:38:38 +0800

    composer require symfony/event-dispatcher

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -8,18 +8,23 @@ use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 
 class Framework
 {
     private $matcher;
     private $controllerResolver;
     private $argumentResolver;
+    private $dispatcher;
 
-    public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
+
+    public function __construct(UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver, EventDispatcher $dispatcher)
     {
-        $this->matcher = $matcher;
+        $this->matcher            = $matcher;
         $this->controllerResolver = $controllerResolver;
-        $this->argumentResolver = $argumentResolver;
+        $this->argumentResolver   = $argumentResolver;
+        $this->dispatcher         = $dispatcher;
     }
 
     public function handle(Request $request)
@@ -30,7 +35,7 @@ class Framework
             $request->attributes->add($this->matcher->match($request->getPathInfo()));
 
             $controller = $this->controllerResolver->getController($request);
-            $arguments = $this->argumentResolver->getArguments($request, $controller);
+            $arguments  = $this->argumentResolver->getArguments($request, $controller);
 
             return call_user_func_array($controller, $arguments);
         } catch (ResourceNotFoundException $exception) {
@@ -38,5 +43,10 @@ class Framework
         } catch (\Exception $exception) {
             return new Response('An error occurred', 500);
         }
+
+        // dispatch a response event
+        $this->dispatcher->dispatch(new ResponseEvent($response, $request), 'response');
+
+        return $response;
     }
 }
--- web/front.php
+++ web/front.php
@@ -1,16 +1,13 @@
 <?php
 
-// framework/front.php
-var_dump(php_ini_loaded_file(), php_ini_scanned_files());
-
-phpinfo();exit;
 require_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
-
 use Symfony\Component\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+
 
 function render_template($request)
 {
@@ -23,18 +20,25 @@ function render_template($request)
 
 
 $request = Request::createFromGlobals();
-$routes = include __DIR__.'/../src/app.php';
+$routes  = include __DIR__.'/../src/app.php';
 $context = new Routing\RequestContext();
 $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 
+$dispatcher = new EventDispatcher();
+$dispatcher->addListener('response', [new Simplex\ContentLengthListener(), 'onResponse'], -255);
+$dispatcher->addListener('response', [new Simplex\GoogleListener(), 'onResponse']);
+
+$controllerResolver = new ControllerResolver();
+$argumentResolver   = new ArgumentResolver();
+
 $controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+$argumentResolver   = new HttpKernel\Controller\ArgumentResolver();
 
 
 $framework = new Simplex\Framework($matcher, $controllerResolver, $argumentResolver);
-$response = $framework->handle($request);
+$response  = $framework->handle($request);
 
 
 $response->send();

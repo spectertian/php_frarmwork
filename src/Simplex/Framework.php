ccddbe0d21b75a1be90858c1803818af50e4c676
tianzhongsheng <tianzhongsheng@lingxi360.com>
2021-02-20 09:47:28 +0800

    new . HttpKernel

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,8 +2,33 @@
 // example.com/src/Simplex/Framework.php
 namespace Simplex;
 
-use Symfony\Component\HttpKernel\HttpKernel;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel;
+use Symfony\Component\Routing;
 
-class Framework extends HttpKernel
+
+class Framework extends HttpKernel\HttpKernel
 {
+    public function __construct($routes)
+    {
+        $context = new Routing\RequestContext();
+        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+        $requestStack = new RequestStack();
+
+        $controllerResolver = new HttpKernel\Controller\ControllerResolver();
+        $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
+
+        $dispatcher = new EventDispatcher();
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(
+            'Calendar\Controller\ErrorController::exception'
+        ));
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
+        $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
+        $dispatcher->addSubscriber(new StringResponseListener());
+
+        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
+    }
 }
+
--- web/front.php
+++ web/front.php
@@ -2,36 +2,12 @@
 // example.com/web/front.php
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
-$requestStack = new RequestStack();
 $routes = include __DIR__.'/../src/app.php';
 
-$context = new Routing\RequestContext();
-$matcher = new Routing\Matcher\UrlMatcher($routes, $context);
+$framework = new Simplex\Framework($routes);
 
-$controllerResolver = new HttpKernel\Controller\ControllerResolver();
-$argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-$listener = new HttpKernel\EventListener\ErrorListener(
-    'Calendar\Controller\ErrorController::exception'
-);
+$framework->handle($request)->send();
 
-$dispatcher = new EventDispatcher();
-$dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-$dispatcher->addSubscriber(new HttpKernel\EventListener\StreamedResponseListener());
-$dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-$dispatcher->addSubscriber(new Simplex\StringResponseListener());
-$dispatcher->addSubscriber($listener);
-
-
-
-$framework = new Simplex\Framework($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-
-$response = $framework->handle($request);
-$response->send();

5bccc605a9e044180e8fe0413ca3fe8fe6a19183
tianzhongsheng <tianzhongsheng@lingxi360.com>
2021-02-20 09:25:25 +0800

    new . HttpKernel

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,53 +2,8 @@
 // example.com/src/Simplex/Framework.php
 namespace Simplex;
 
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpKernel\HttpKernel;
 
-
-class Framework implements HttpKernelInterface
+class Framework extends HttpKernel
 {
-    private $matcher;
-    private $controllerResolver;
-    private $argumentResolver;
-    private $dispatcher;
-
-
-    public function __construct(EventDispatcher $dispatcher, UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver)
-    {
-        $this->matcher            = $matcher;
-        $this->controllerResolver = $controllerResolver;
-        $this->argumentResolver   = $argumentResolver;
-        $this->dispatcher         = $dispatcher;
-    }
-
-    public function handle(
-        Request $request, $type = HttpKernelInterface::MASTER_REQUEST, $catch = true
-    ) {
-        $this->matcher->getContext()->fromRequest($request);
-
-        try {
-            $request->attributes->add($this->matcher->match($request->getPathInfo()));
-
-            $controller = $this->controllerResolver->getController($request);
-            $arguments  = $this->argumentResolver->getArguments($request, $controller);
-
-            return call_user_func_array($controller, $arguments);
-        } catch (ResourceNotFoundException $exception) {
-            return new Response('Not Found', 404);
-        } catch (\Exception $exception) {
-            return new Response('An error occurred', 500);
-        }
-
-        // dispatch a response event
-        $this->dispatcher->dispatch(new ResponseEvent($response, $request), 'response');
-
-        return $response;
-    }
 }

4a0d0aca9b9376bd7dd2470af81e6c5ace917690
tianzhongsheng <tianzhongsheng@lingxi360.com>
2021-02-20 10:05:50 +0800

    new . HttpKernel

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -2,33 +2,10 @@
 // example.com/src/Simplex/Framework.php
 namespace Simplex;
 
-use Symfony\Component\EventDispatcher\EventDispatcher;
-use Symfony\Component\HttpFoundation;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpKernel;
-use Symfony\Component\Routing;
+use Symfony\Component\HttpKernel\HttpKernel;
 
-
-class Framework extends HttpKernel\HttpKernel
+class Framework extends HttpKernel
 {
-    public function __construct($routes)
-    {
-        $context = new Routing\RequestContext();
-        $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
-        $requestStack = new RequestStack();
-
-        $controllerResolver = new HttpKernel\Controller\ControllerResolver();
-        $argumentResolver = new HttpKernel\Controller\ArgumentResolver();
-
-        $dispatcher = new EventDispatcher();
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\ErrorListener(
-            'Calendar\Controller\ErrorController::exception'
-        ));
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\RouterListener($matcher, $requestStack));
-        $dispatcher->addSubscriber(new HttpKernel\EventListener\ResponseListener('UTF-8'));
-        $dispatcher->addSubscriber(new StringResponseListener());
-
-        parent::__construct($dispatcher, $controllerResolver, $requestStack, $argumentResolver);
-    }
 }
+
 
